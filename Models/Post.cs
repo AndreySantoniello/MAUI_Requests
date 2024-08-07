938c1b7cc4eb38817e1854165ead4c780b0617ff
Andrey Santoniello <andreysantoniello@gmail.com>
2024-08-07 17:16:34 -0300

    chore: post model entity

---
+++ Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTPClient.Models
+{
+    public class Post
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Body { get; set; }
+    }
+}

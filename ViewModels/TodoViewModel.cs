469fdf02f1df8749e2823800c55b013c84a18a56
Andrey Santoniello <andreynardy@gmail.com>
2024-09-11 18:13:55 -0300

    chore: todo view & viewmodel

---
+++ ViewModels/TodoViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using HTTPClient.Models;
+using HTTPClient.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace HTTPClient.ViewModels
+{
+    internal partial class TodoViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        ObservableCollection<Todo>? todos;
+
+        public ICommand getTodosCommand { get; set; }
+
+        public TodoViewModel()
+        {
+            getTodosCommand = new Command(getTodos);
+        }
+
+        public async void getTodos()
+        {
+            TodoService todoService = new();
+            Todos = await todoService.FetchTodosAsync();
+        }
+    }
+}
